6157b01814f46261963b2ec0471c56777a26f4af
Juyeob Lee <lcg0116@gmail.com>
2026-07-10 04:00:01 +0900

    fix(monster): Swarfarm 동기화를 위해 몬스터 이름 중복 허용

--- Siege-Battle-Manager/backend/siege-backend/src/main/java/com/sbm/siegebackend/domain/monster/Monster.java
+++ Siege-Battle-Manager/backend/siege-backend/src/main/java/com/sbm/siegebackend/domain/monster/Monster.java
@@ -22,7 +22,7 @@ public class Monster {
     @Column(nullable = false, unique = true, length = 100)
     private String code;
 
-    @Column(nullable = false, unique = true, length = 100)
+    @Column(nullable = false, length = 100)
     private String name;
 
     @Enumerated(EnumType.STRING)
