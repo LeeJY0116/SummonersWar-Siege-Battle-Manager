c26d609cd5cba6e15975b9f9e16b0803b69e64ac
Juyeob Lee <lcg0116@gmail.com>
2026-07-13 16:59:15 +0900

    test: 테스트 전용 H2 설정 추가

--- Siege-Battle-Manager/backend/siege-backend/src/test/java/com/sbm/siegebackend/SiegeBackendApplicationTests.java
+++ Siege-Battle-Manager/backend/siege-backend/src/test/java/com/sbm/siegebackend/SiegeBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.sbm.siegebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SiegeBackendApplicationTests {
 
 	@Test
--- Siege-Battle-Manager/backend/siege-backend/src/test/java/com/sbm/siegebackend/domain/user/UserServiceSignUpTest.java
+++ Siege-Battle-Manager/backend/siege-backend/src/test/java/com/sbm/siegebackend/domain/user/UserServiceSignUpTest.java
@@ -12,12 +12,14 @@ import com.sbm.siegebackend.domain.user.dto.UserLoginRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class UserServiceSignUpTest {
 
